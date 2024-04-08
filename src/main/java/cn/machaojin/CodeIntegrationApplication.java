83da990e26898ff8dd31f2744e5337fd9818b7ca
machaojin <1917939763@qq.com>
2024-04-08 15:35:12 +0800

    初始化项目

---
+++ src/main/java/cn/machaojin/CodeIntegrationApplication.java
@@ -0,0 +1,13 @@
+package cn.machaojin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/machaojin/CodeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.machaojin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
