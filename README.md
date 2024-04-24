60986b0259ae4a732dfd6a59da1dfa78057dbd39
machaojin <1917939763@qq.com>
2024-04-24 18:42:41 +0800

    【DEVELOP-002】测试代码GitLab Runner是否能正常运行

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ## 代码开发日志
 * DEVELOP-001（2024年4月8日）：初始化项目，构建开发环境，前期准备工作
 * DEVELOP-002（2024年4月9日）：搭建GitLab Runner，每次提交代码通过GitLab Runner上传结果到SonarQube
+* DEVELOP-002（2024年4月24日）：测试代码GitLab Runner是否能正常运行
 
 
 ## 开发过程重要代码
--- src/main/java/cn/machaojin/CodeIntegrationApplication.java
+++ src/main/java/cn/machaojin/CodeIntegrationApplication.java
@@ -8,7 +8,7 @@ public class CodeIntegrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodeIntegrationApplication.class, args);
-        System.out.println("启动成功！");
+        System.out.println("启动成功！！！！");
     }
 
 }
